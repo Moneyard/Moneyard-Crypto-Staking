324f81417ff4b5e802ac986ed9fd9e61a4fc99ba
Moneyard <basiwelholisticventures@gmail.com>
2025-04-06 01:38:38 +0200

    Update server.js with dashboard route and static file handling

--- server.js
+++ server.js
@@ -5,9 +5,14 @@ const app = express();
 // Serve static files from the 'public' folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve the homepage
+// Serve the homepage (Login and Signup)
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+// Example Dashboard Route (after successful login)
+app.get('/dashboard', (req, res) => {
+    res.send('Welcome to your dashboard!');
 });
 
 // Start the server
