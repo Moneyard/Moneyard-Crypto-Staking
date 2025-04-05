20c911390a447fefb07cd559d370935167ab5288
Moneyard <basiwelholisticventures@gmail.com>
2025-04-05 11:43:00 +0200

    git add routes/authRoutes.js git commit -m "fix: authRoutes for SQLite backend" git push origin main

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
+// User registration route
 router.post('/signup', authController.signup);
+
+// User login route
 router.post('/login', authController.login);
 
 module.exports = router;
